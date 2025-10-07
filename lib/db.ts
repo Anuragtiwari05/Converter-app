fab000056795f11a1c00f4b12e7ac486c639e55e
Anurag <aanuragtiwari10a@gmail.com>
2025-10-08 01:14:12 +0530

    debug

--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,22 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI : string = process.env.MONGODB_URI as string;
-
-if(!MONGODB_URI){
-    throw new Error("please define the uri")
-}
+let isConnected = false;
 
 export const connectDB = async () => {
   try {
-    if (mongoose.connections[0].readyState) {
+    if (isConnected || mongoose.connections[0].readyState) {
       console.log("✅ Already connected to DB");
+      isConnected = true;
       return;
     }
-    await mongoose.connect(process.env.MONGODB_URI!);
+
+    const MONGODB_URI = process.env.MONGODB_URI;
+    if (!MONGODB_URI) {
+      throw new Error("please define the MONGODB_URI in your .env file");
+    }
+
+    await mongoose.connect(MONGODB_URI);
+    isConnected = true;
     console.log("✅ Connected to MongoDB");
   } catch (error) {
     console.error("❌ DB connection error:", error);
